3b1fc76e07ffb7f87d11eff95d89bbcf0c1e8a11
Jitendra Alekar <jitendraalekar@gmail.com>
2021-08-07 22:29:06 +0530

    Add about
    Signed-off-by: Jitendra Alekar <jitendraalekar@gmail.com>

---
+++ app/src/main/java/com/jitendraalekar/todo/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.jitendraalekar.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jitendraalekar.todo.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbar.title = getString(R.string.app_name)
+        binding.webview.loadUrl("file:///android_asset/about.html")  //android_asset points to assets
+    }
+}
